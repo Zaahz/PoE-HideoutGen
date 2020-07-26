db9e1d82574e5d5df2250c6eb84d774ffd40ce84
Zahz <martineggemark@gmail.com>
2020-07-26 18:50:53 +0200

    Create Hideout.py

---
+++ Hideout.py
@@ -0,0 +1,70 @@
+print("Language = \"English\"", file=open("python_hideout.hideout", "a"))
+print("Hideout Name = \"Celestial Nebula Hideout\"", file=open("python_hideout.hideout", "a"))
+print("Hideout Hash = 35022\n", file=open("python_hideout.hideout", "a"))
+print("Stash = { Hash=3230065491, X=420, Y=363, Rot=49077, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Guild Stash = { Hash=139228481, X=393, Y=390, Rot=64651, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Waypoint = { Hash=1224707366, X=362, Y=330, Rot=0, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Crafting Bench = { Hash=2059629901, X=409, Y=380, Rot=7613, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Map Device = { Hash=2306038834, X=327, Y=333, Rot=33349, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Navali = { Hash=693228958, X=359, Y=390, Rot=62814, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Einhar = { Hash=2684274993, X=427, Y=322, Rot=41654, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Alva = { Hash=2115859440, X=386, Y=281, Rot=36209, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Helena = { Hash=845403974, X=422, Y=348, Rot=52209, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Niko = { Hash=2906227343, X=424, Y=335, Rot=52192, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Jun = { Hash=3992724805, X=342, Y=280, Rot=40585, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Zana = { Hash=3506797600, X=428, Y=308, Rot=41654, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Sister Cassia = { Hash=10623884, X=308, Y=388, Rot=2058, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Tane Octavius = { Hash=4285419720, X=334, Y=386, Rot=320, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Kirac = { Hash=2913423765, X=429, Y=291, Rot=43653, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Twisted Building Supplies = { Hash=3347678408, X=330, Y=397, Rot=32451, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=273, Y=289, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=275, Y=261, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=279, Y=241, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=320, Y=239, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=369, Y=239, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=413, Y=239, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=458, Y=240, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=456, Y=282, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=452, Y=319, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=449, Y=362, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=450, Y=388, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=448, Y=421, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Ground = { Hash=1798490749, X=307, Y=389, Rot=0, Flip=0, Var=5 }", file=open("python_hideout.hideout", "a"))
+print("Grass Ground = { Hash=1798490749, X=307, Y=320, Rot=0, Flip=0, Var=5 }", file=open("python_hideout.hideout", "a"))
+print("Grass Ground = { Hash=1798490749, X=376, Y=389, Rot=0, Flip=0, Var=5 }", file=open("python_hideout.hideout", "a"))
+print("Grass Ground = { Hash=1798490749, X=376, Y=320, Rot=0, Flip=0, Var=5 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=405, Y=421, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=364, Y=422, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=315, Y=421, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=273, Y=421, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=274, Y=391, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=275, Y=371, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Grass Patch = { Hash=3856837925, X=275, Y=334, Rot=0, Flip=0, Var=4 }", file=open("python_hideout.hideout", "a"))
+print("Goddess Statue = { Hash=2929688420, X=423, Y=396, Rot=56929, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+print("Seed Stockpile = { Hash=2964547131, X=374, Y=388, Rot=64268, Flip=0, Var=0 }", file=open("python_hideout.hideout", "a"))
+
+
+
+row = 1
+
+
+y = 220
+while row <= 23:
+    x = 250
+    while x <= 470: 
+        #child = f"Oriathan Child = \{ Hash=2808502392, X={x}, Y={y}, Rot=0, Flip=0, Var=10 \}"
+        print(f"Oriathan Child = {{ Hash=2808502392, X={x}, Y={y}, Rot=0, Flip=0, Var=0 }}", file=open("python_hideout.hideout", "a"))
+        x = x+10
+        print("X = ", x)
+    y = y+10 #Add 10 to y to go up one row
+    print("\nY = ", y)
+    row += 1
+
+
+
+
+
+
+
+"Oriathan Child = { Hash=2808502392, X=360, Y=350, Rot=0, Flip=0, Var=10 }"
+    
